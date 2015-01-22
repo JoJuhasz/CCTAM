0e0767737f6dd58ecd843062cd3b519f2a8b2b3a
Jonathan Juhasz <jonathan.juhasz@gmail.com>
2015-01-22 20:43:32 +0100

    Enable FOS Kernel + Basic Setup Security
    Enable FOSUser in Kernel and add basic Securiy configuration. Create
    User Entity in CctaUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Ccta\CoreBundle\CctaCoreBundle(),
             new Ccta\UserBundle\CctaUserBundle(),
+	        new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Ccta/UserBundle/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ccta\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="ccta_user")
+ * @ORM\Entity(repositoryClass="Ccta\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+	/**
+	 * @var integer
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+}
